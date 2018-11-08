4e0ff2f968337e100435212a24c4d20ebedb24d0
mpj <maple.develops@gmail.com>
2018-11-07 21:39:28 -0500

    Comment out delegate method and add RxSwift method with the same outcome

--- DelegateTutorial/ClassAVC.swift
+++ DelegateTutorial/ClassAVC.swift
@@ -7,10 +7,32 @@
 //
 
 import UIKit
+import RxSwift
 
 //MARK: step 4 conform the protocol here
-class ClassAVC: UIViewController, ClassBVCDelegate {
+//class ClassAVC: UIViewController, ClassBVCDelegate {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        //MARK: step 5 create a reference of Class B and bind them through the prepareforsegue method
+//        if let nav = segue.destination as? UINavigationController, let classBVC = nav.topViewController as? ClassBVC {
+//            classBVC.delegate = self
+//        }
+//    }
+//
+//    //MARK: step 6 finally use the method of the contract
+//    func changeBackgroundColor(_ color: UIColor?) {
+//        view.backgroundColor = color
+//    }
+//}
+
+class ClassAVC: UIViewController {
     
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +40,16 @@ class ClassAVC: UIViewController, ClassBVCDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        //MARK: step 5 create a reference of Class B and bind them through the prepareforsegue method
         if let nav = segue.destination as? UINavigationController, let classBVC = nav.topViewController as? ClassBVC {
-            classBVC.delegate = self
+            classBVC.changeBackgroundColor
+                .subscribe(onNext: { [weak self] newBackgroundColor in
+                    self?.changeBackgroundColor(newBackgroundColor)
+                })
+                .disposed(by: disposeBag)
         }
     }
     
-    //MARK: step 6 finally use the method of the contract
     func changeBackgroundColor(_ color: UIColor?) {
         view.backgroundColor = color
     }
-
-    
-    
-    
 }
