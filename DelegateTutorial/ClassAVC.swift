25de08fe82c77e991bf05afac95fafb5cbc5a236
mpj <maple.develops@gmail.com>
2018-11-07 21:06:54 -0500

    Revert "Pod updates for RxSwift"
    This reverts commit d9f63ee7585bc1bd9812004b8aa547959de808fd.

--- DelegateTutorial/ClassAVC.swift
+++ DelegateTutorial/ClassAVC.swift
@@ -11,6 +11,7 @@ import UIKit
 //MARK: step 4 conform the protocol here
 class ClassAVC: UIViewController, ClassBVCDelegate {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,4 +28,8 @@ class ClassAVC: UIViewController, ClassBVCDelegate {
     func changeBackgroundColor(_ color: UIColor?) {
         view.backgroundColor = color
     }
+
+    
+    
+    
 }
